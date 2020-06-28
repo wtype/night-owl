468503f29d6aea2c45dd996a96b11a88d31a4ae2
wusker <s.bergellio@gmail.com>
2020-06-28 00:31:52 -0600

    Refactoring

--- owl.js
+++ owl.js
@@ -1,24 +1,5 @@
-const darkenImages = () => [...document.images].forEach(image => image.style.filter = `brightness(0.75)`);
-
-function calculateBrightness(color) {
-  const colorValues = [];
-
-  let cleanColor = color.replace(/[^0-9,]/g, '');
-  // slicing at ends to remove surrounding parentheses 
-  cleanColor = cleanColor.slice(1, cleanColor.length - 1).split(',').slice(0, 3);
-  cleanColor.forEach(value => {
-    value.trim();
-    colorValues.push(+value);
-  });
-
-  if (colorValues.length !== 3) {
-    throw new Error(`Needing three color values to be passed in to calculate brightness. Received: ${colorValues.length}`);
-  }
-
-  const brightness = Number(Math.sqrt(0.241 * (colorValues[0] ** 2) + 0.691 * (colorValues[1] ** 2) + 0.068 * (colorValues[2] ** 2)).toFixed(2));
-
-  return [color, brightness];
-}
+const darkenImages = () =>
+  [...document.images].forEach(image => (image.style.filter = `brightness(0.75)`));
 
 function getColors() {
   const colors = [];
@@ -40,12 +21,46 @@ function getColors() {
   return colors;
 }
 
+function calculateBrightness(color) {
+  const colorValues = [];
+
+  let cleanColor = color.replace(/[^0-9,]/g, '');
+  // slicing at ends to remove surrounding parentheses
+  cleanColor = cleanColor
+    .slice(1, cleanColor.length - 1)
+    .split(',')
+    .slice(0, 3);
+
+  cleanColor.forEach(value => {
+    value.trim();
+    colorValues.push(+value);
+  });
+
+  if (colorValues.length !== 3) {
+    throw new Error(
+      `Needing three color values to be passed in to calculate brightness. Received: ${colorValues.length}`
+    );
+  }
+
+  const brightness = Number(
+    Math.sqrt(
+      0.241 * colorValues[0] ** 2 + 0.691 * colorValues[1] ** 2 + 0.068 * colorValues[2] ** 2
+    ).toFixed(2)
+  );
+
+  return [color, brightness];
+}
+
+function replaceColor(c) {
+  const [color, brightness] = c;
+  console.log(color, brightness);
+}
+
 function go() {
   const colors = getColors();
-
-  for (const color of colors) {
-    console.log(calculateBrightness(color));
-  }
+  const withBrightness = [];
+  colors.forEach(color => withBrightness.push(calculateBrightness(color)));
+  withBrightness.forEach(color => replaceColor(color));
 }
 
 go();
