55b61261a193d428e2d7cba45b636bcf0bd60283
wusker <s.bergellio@gmail.com>
2020-07-06 22:36:02 -0600

    Regex Color Gathering

--- owl.js
+++ owl.js
@@ -1,17 +1,24 @@
 function getColors() {
-  const hexRegex = '#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\\b'
-  const hslRegex = 'hsl\(\s*(\d+)\s*,\s*(\d*(?:\.\d+)?%)\s*,\s*(\d*(?:\.\d+)?%)\)';
-  const hslaRegex = 'hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%,\s*(\d*(?:\.\d+)?)\)';
-  const rgbaRegex = 'rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)';
-  const rgbRegex = 'rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)';
+  const hexRegex = /^#([a-f0-9]{3}|[a-f0-9]{4}(?:[a-f0-9]{2}){0,2})\b$/;
+  const hslRegex = /^hsl\(\s*(\d+)\s*,\s*(\d*(?:\.\d+)?%)\s*,\s*(\d*(?:\.\d+)?%)\)$/;
+  const hslaRegex = /^hsla\((\d+),\s*([\d.]+)%,\s*([\d.]+)%,\s*(\d*(?:\.\d+)?)\)$/;
+  const rgbaRegex = /^rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)$/;
+  const rgbRegex = /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/;
+  // need to check which browsers coerce colors to rgb and rgba
+  const allColorRegex =
+    hexRegex.source + '|' +
+    rgbRegex.source + '|' +
+    rgbaRegex.source + '|' +
+    hslRegex.source + '|' +
+    hslaRegex.source;
 
-  const allColorRegex = hexRegex + '|' + rgbRegex + '|' + rgbaRegex + '|' + hslRegex + '|' + hslaRegex;
   const fullColorRegex = new RegExp(allColorRegex, 'ig');
 
   const colors = [];
-  const elements = document.getElementsByTagName('*');
+  const elements = {};
+  const allElements = document.getElementsByTagName('*');
 
-  for (const element of elements) {
+  for (const element of allElements) {
     elementStyles = window.getComputedStyle(element);
 
     for (const style of elementStyles) {
@@ -19,16 +26,23 @@ function getColors() {
 
       if (value && value.match(fullColorRegex) && !colors.includes(value)) {
         colors.push(value);
+        if (!elements[element.localName]) {
+          elements[element.localName] = value;
+        }
       }
     }
   }
 
-  return colors;
+  return [colors, elements];
 }
 
 function calculateBrightness(color) {
   const colorValues = [];
 
+  /* TODO:
+    check which browsers coerce colors 
+    to rgb/rgba then see about cleaning 
+  */
   let cleanColor = color.replace(/[^0-9,]/g, '');
   // slicing at ends to remove surrounding parentheses
   cleanColor = cleanColor
@@ -64,7 +78,10 @@ const darkenImages = () =>
   [...document.images].forEach(image => (image.style.filter = `brightness(0.75)`));
 
 function go() {
-  const colors = getColors();
+  const colors = getColors()[0];
+  const elements = getColors()[1];
+  console.log(elements);
+
   const withBrightness = [];
   colors.forEach(color => withBrightness.push(calculateBrightness(color)));
   withBrightness.forEach(color => replaceColor(color));
